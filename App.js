17940732c41e2b86ea0ee13f6028b1ae05f56352
Luquete <lucas457856@gmail.com>
2026-06-11 13:31:37 -0300

    mudei a splash art

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { useState } from 'react';
 import { View } from 'react-native';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import './global.css';
 
-import { ThemeProvider } from './src/context/ThemeContext';
+import SplashVideo from './SplashVideo';
 import AppNavigator from './src/navigation/AppNavigator';
 
+// 👇 IMPORTA O PROVIDER
+import { ThemeProvider } from './src/context/ThemeContext';
+
 export default function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  // 👇 SPLASH PRIMEIRO (não precisa de Theme aqui)
+  if (showSplash) {
+    return <SplashVideo onFinish={() => setShowSplash(false)} />;
+  }
+
+  // 👇 APP NORMAL COM THEME
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider>
--- App2.js
+++ App2.js
@@ -1,25 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { useState } from 'react';
 import { View } from 'react-native';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import './global.css';
 
-import SplashVideo from './SplashVideo';
+import { ThemeProvider } from './src/context/ThemeContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
-// 👇 IMPORTA O PROVIDER
-import { ThemeProvider } from './src/context/ThemeContext';
-
 export default function App() {
-  const [showSplash, setShowSplash] = useState(true);
-
-  // 👇 SPLASH PRIMEIRO (não precisa de Theme aqui)
-  if (showSplash) {
-    return <SplashVideo onFinish={() => setShowSplash(false)} />;
-  }
-
-  // 👇 APP NORMAL COM THEME
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <ThemeProvider>
